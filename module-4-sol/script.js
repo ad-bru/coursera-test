922aa730dd6dbcb526bfea86bc43c1098bcc5029
ad-bru <90262431+ad-bru@users.noreply.github.com>
2022-01-09 22:21:22 +0100

    Update script.js

--- module-4-sol/script.js
+++ module-4-sol/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "Jean", "Janice", "Jake", "Papa", "Fitzgerald", "Louis", "Pops", "Lucie", "Jeremy"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
